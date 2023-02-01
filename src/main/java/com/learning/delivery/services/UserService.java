02f08e7e92de98de3878b74ed4a05e111a7318d3
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-01 09:54:09 -0300

    Service layer, component registration

---
+++ src/main/java/com/learning/delivery/services/UserService.java
@@ -0,0 +1,25 @@
+package com.learning.delivery.services;
+
+import com.learning.delivery.entities.User;
+import com.learning.delivery.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
